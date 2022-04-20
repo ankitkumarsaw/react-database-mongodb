34f82733672b75ed82ea09dd68e17af209b9dc59
Ankit Kumar Saw <55346806+ankitkumarsaw@users.noreply.github.com>
2022-04-21 04:09:00 +0530

    Update backend

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use('/user',userRoutes);
 
 app.use(express.static(__dirname + '/public'))
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
